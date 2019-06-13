37a1de06164b7aca9d1178348d4cb006c29e2c66
mahendra <mahendra@arraigntech.com>
2019-06-13 19:32:52 +0530

    timer functionality and ui done

---
+++ src/Stopwatch.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import "./App.css";
+
+export default class Stopwatch extends Component {
+    state = {
+        timerOn: false,
+        timerStart: 0,
+        timerTime: 0
+    }
+    startTimer = () => {
+        this.setState({
+            timerOn: true,
+            timerTime: this.state.timerTime,
+            timerStart: Date.now() - this.state.timerTime
+        });
+        this.timer = setInterval(() => { this.setState({ timerTime: Date.now() - this.state.timerStart }) }, 10);
+    }
+    stopTimer = () => {
+        this.setState({ timerOn: false });
+        clearInterval(this.timer);
+    }
+    resetTimer = () => {
+        this.setState({
+            timerStart: 0,
+            timerTime: 0
+        });
+    }
+    render() {
+        const { timerTime, timerOn } = this.state;
+        let centiseconds = ("0" + (Math.floor(timerTime / 10) % 100)).slice(-2);
+        let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
+        let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
+        let hours = ("0" + Math.floor(timerTime / 3600000)).slice(-2);
+        return (
+            <div className="Stopwatch">
+                <div className="Stopwatch-header">
+                    Stopwatch
+                </div>
+                <div className="Stopwatch-display">
+                    {hours} : {minutes} : {seconds} : {centiseconds}
+                    {timerOn === false && timerTime === 0 && (<button onClick={this.startTimer}>Start</button>)}
+                    {timerOn === true && (<button onClick={this.stopTimer}>Stop</button>)}
+                    {timerOn === false && timerTime > 0 && (<button onClick={this.startTimer}>Resume</button>)}
+                    {timerOn === false && timerTime > 0 && (<button onClick={this.resetTimer}>Reset</button>)}
+                </div>
+
+            </div>
+        )
+    }
+}
